053b473a4009c1f555c57745df821115f6388e7f
Vaishnavi Pataskar <vrpataskar@gmail.com>
2026-04-14 12:20:28 +0530

    updated the api file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://servease-backend-lqpn.onrender.com/api',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 });
